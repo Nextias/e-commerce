430292f4ceb17c99f2498afe9cd69849a7d8c1d5
Alexander Dmitriev <accnext1997@gmail.com>
2025-03-10 02:49:33 +0300

    validation

--- app/forms/CreateCategoryForm.py
+++ app/forms/CreateCategoryForm.py
@@ -1,9 +1,19 @@
+import sqlalchemy as sa
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+
+from app import db
+from app.models import Category  # type: ignore[name-defined]
 
 
 class CreateCategoryForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired(),
                                                Length(max=64)])
     submit = SubmitField('Добавить')
+
+    def validate_name(self, name):
+        user = db.session.scalar(
+            sa.select(Category).where(Category.name == name.data))
+        if user is not None:
+            raise ValidationError('Используйте другое название категории.')
